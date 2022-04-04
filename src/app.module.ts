07c0388add36644d590f12533f11549bedb6adec
kosuke-17 <tamutamu17@icloud.com>
2022-04-04 16:30:46 +0900

    不要なファイル削除

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 import { GraphQLModule } from '@nestjs/graphql';
@@ -15,15 +13,11 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
     DatabaseModule,
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      // playground: process.env.NODE_ENV === 'development' ? true : false,
-      // debug: process.env.NODE_ENV === 'development' ? true : false,
       debug: true,
       playground: true,
       autoSchemaFile: true,
     }),
     ComponentsModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
