45971b9d03f5085f80ce43c2f3c3eca5bf1422c2
kosuke-17 <tamutamu17@icloud.com>
2022-04-04 09:22:01 +0900

    nestjsのapollo導入

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,24 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ComponentsModule } from './components/components.module';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
   // 環境変数を利用する
-  imports: [ConfigModule.forRoot(), DatabaseModule],
+  // ローカル環境だったらplaygroundを利用する
+  imports: [
+    ConfigModule.forRoot(),
+    DatabaseModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: (process.env.NODE_ENV === 'development' && true) || false,
+      debug: (process.env.NODE_ENV === 'development' && true) || false,
+      autoSchemaFile: true,
+    }),
+    ComponentsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
