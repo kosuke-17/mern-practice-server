62402c1320715eb3ddf517a9d88aaad084b3a875
kosuke-17 <tamutamu17@icloud.com>
2022-04-05 12:57:28 +0900

    利用したsqlの記述

---
+++ skills.sql
@@ -0,0 +1,17 @@
+-- データを追加
+insert into skills (name, img, content, stackTime) VALUES("Typescript","typescript.png","Typescript", 10);
+insert into skills (name, img, content, stackTime) VALUES("JavaScript","javascript.png","JavaScript", 20);
+insert into skills (name, img, content, stackTime) VALUES("React","react.png","Reactの説明", 50);
+insert into skills (name, img, content, stackTime) VALUES("Nextjs","nextjs.png","Nextjsの説明", 90);
+insert into skills (name, img, content, stackTime) VALUES("Node.js","nodejs.png","Nodejsの説明", 110);
+insert into skills (name, img, content, stackTime) VALUES("Express","express.png","css3の説明", 50);
+insert into skills (name, img, content, stackTime) VALUES("NestJS","nest.png","css3の説明", 50);
+insert into skills (name, img, content, stackTime) VALUES("GraphQL","graphql.png","GraphQLの説明", 210);
+insert into skills (name, img, content, stackTime) VALUES("Apollo","apollo.png","apolloの説明", 220);
+insert into skills (name, img, content, stackTime) VALUES("HTML","html.png","html5の説明", 150);
+insert into skills (name, img, content, stackTime) VALUES("CSS","css.png","css3の説明", 50);
+insert into skills (name, img, content, stackTime) VALUES("Jest","jest.png","css3の説明", 50);
+
+
+-- データを編集
+update skills set img="javascript.png" where id="2d2f2aed-d188-4eac-be1a-cbdf48823e37";
